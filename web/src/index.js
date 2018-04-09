3d704aa0976186ff5a4b98bb22b6c32b5cc5c37a
jinjunjie <jay@jinjunjies-MacBook-Pro.local>
2018-04-09 14:05:37 +0800

    create web

---
+++ web/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function Index(props) {
+    return (
+        <div>Hello React!</div>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
---
+++ web/webpack.prod.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.css$/,
+
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			}
+		]
+	},
+
+	plugins: [
+		new UglifyJSPlugin(),
+		new ExtractTextPlugin('style.[contentHash].css'),
+        new HtmlWebPackPlugin({
+            filename:'index.html',
+            template:'src/index.html',
+            title:'My React Webpack Demo'
+        })
+	]
+};
